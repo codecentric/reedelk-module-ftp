3f4da04ed1c6d2b8f1c5aa395f7f6a9ae13ad5c8
lorenzo <info@reedelk.com>
2020-04-21 10:09:25 +0200

    Bugfix: store did not use binary data transfer.

--- src/main/java/com/reedelk/ftp/internal/CommandStore.java
+++ src/main/java/com/reedelk/ftp/internal/CommandStore.java
@@ -1,5 +1,6 @@
 package com.reedelk.ftp.internal;
 
+import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 
 import java.io.IOException;
@@ -17,6 +18,7 @@ public class CommandStore implements Command<Boolean> {
 
     @Override
     public Boolean execute(FTPClient client) throws IOException {
+        client.setFileType(FTP.BINARY_FILE_TYPE);
         return client.storeFile(remoteFilePath, inputStream);
     }
 }
